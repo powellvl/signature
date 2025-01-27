73a03da7f953f4334a8aee8a13b5fadc359c52fe
powellvl <lucax.mb03@gmail.com>
2025-01-27 16:37:40 +0100

    fix login

--- controller/userController.js
+++ controller/userController.js
@@ -1,38 +1,44 @@
 const User = require("../models/Users.js");
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-exports.signUpUser = (req, res, next) => {
-    bcrypt
-      .hash(req.body.password, 10)
-      .then((hash) => {
-        const user = new User({
-          name: req.body.name,
-          password: hash,
-        });
-        user
-          .save()
-          .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-          .catch((error) => res.status(400).json({ error }));
-      })
-      .catch((error) => res.status(500).json({ error }));
+exports.createUser = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        name: req.body.name,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.loginUser = (req, res, next) => {
   User.findOne({ name: req.body.name })
     .then((user) => {
       if (user === null) {
-        res.status(401).json({ message: "Paire login/mot de passe incorrecte" });
+        res.status(401).json({
+          message: "Paire login/mot de passe incorrecte",
+        });
       } else {
         bcrypt
           .compare(req.body.password, user.password)
           .then((valid) => {
             if (!valid) {
-              res.status(401).json({ message: "Paire login/mot de passe incorrecte" });
+              res.status(401).json({
+                message: "Paire login/mot de passe incorrecte",
+              });
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", { expiresIn: "24h" }),
+                token: jwt.sign({ userId: user._id }, "ISSOU", {
+                  expiresIn: "24h",
+                }),
               });
             }
           })
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const http = require("http");
 const app = require("./app");
+require("dotenv").config();
 
 const normalizePort = (val) => {
-    const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 };
 
 const port = normalizePort({}.PORT || "3000");
@@ -20,36 +21,35 @@ const port = normalizePort({}.PORT || "3000");
 app.set("port", port);
 
 const errorHandler = (error) => {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-    const address = server.address();
-    const bind =
-        typeof address === "string" ? "pipe " + address : "port: " + port;
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
 
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges.");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use.");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 
 const server = http.createServer(app);
 
 server.on("error", errorHandler);
 server.on("listening", () => {
-    const address = server.address();
-    const bind =
-        typeof address === "string" ? "pipe " + address : "port " + port;
-    console.log("Listening on " + bind);
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
 server.listen(port);
